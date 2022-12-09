484741050260b3e6059d67f0b83fc9546ba39ede
Luis Zubia <soyzubia@gmail.com>
2022-12-09 11:35:42 -0600

    Add function toTitleCase to Book class, so you can get the name of a book like a title.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // We'll disable eslint rule that avoids
 // having multiple classes in one file
-/* eslint-disable max-classes-per-file, class-methods-use-this */
+/* eslint-disable max-classes-per-file, class-methods-use-this, no-extended-native */
 
 // -- (c) Book - class to create valid Books -- //
 class Book {
@@ -12,15 +12,56 @@ class Book {
   // -- createLi - returns an <li> with the book info -- //
   createLi() {
     const templateBook = `
-    <span>${this.title}</span><span> by ${this.author}</span>
-    <button type="button">Remove</button>
-    <hr>
-    `;
+    <span class="book-title">${this.toTitleCase()}</span>
+    <span>by</span>
+    <span class="book-author">${this.author}</span>
+    <button type="button">Remove</button>`;
     const liBook = document.createElement('li');
     liBook.innerHTML = templateBook;
     const bookFragment = document.createDocumentFragment();
     bookFragment.appendChild(liBook);
     return bookFragment;
+  }
+
+  /* To Title Case © 2018 David Gouch | https://github.com/gouch/to-title-case */
+  toTitleCase() {
+    const smallWords = /^(a|an|and|as|is|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
+    const alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
+    const wordSeparators = /([ :–—-])/;
+
+    return this.title.toLowerCase().split(wordSeparators)
+      .map((current, index, array) => {
+        if (
+          /* Check for small words */
+          current.search(smallWords) > -1 &&
+          /* Skip first and last word */
+          index !== 0 &&
+          index !== array.length - 1 &&
+          /* Ignore title end and subtitle start */
+          array[index - 3] !== ':' &&
+          array[index + 1] !== ':' &&
+          /* Ignore small words that start a hyphenated phrase */
+          (array[index + 1] !== '-' ||
+            (array[index - 1] === '-' && array[index + 1] === '-'))
+        ) {
+          return current.toLowerCase();
+        }
+
+        /* Ignore intentional capitalization */
+        if (current.substr(1).search(/[A-Z]|\../) > -1) {
+          return current;
+        }
+
+        /* Ignore URLs */
+        if (array[index + 1] === ':' && array[index + 2] !== '') {
+          return current;
+        }
+
+        /* Capitalize the first letter */
+        return current.replace(alphanumericPattern,
+          (match) => match.toUpperCase());
+      })
+      .join('');
   }
 }
 
